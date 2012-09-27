12621f20b02894165e80058cca7ba6e8f73dbc33
sei <sei@sei-MBA.local>
2012-09-27 21:22:27 +0900

    Add TimelineViewController

---
+++ TwitterApp/TimelineViewController.h
@@ -0,0 +1,14 @@
+//
+//  TimelineViewController.h
+//  TwitterApp
+//
+//  Created by sei on 2012/09/27.
+//  Copyright (c) 2012年 sei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TimelineViewController : UITableViewController
+- (IBAction)pressComposeButton:(id)sender;
+
+@end
