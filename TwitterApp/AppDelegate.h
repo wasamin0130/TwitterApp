c0e7b0ca60b9f5c6ab271f5e2d6c683568aa427d
sei <sei@sei-MBA.local>
2012-09-27 18:41:18 +0900

    Initial Commit

---
+++ TwitterApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TwitterApp
+//
+//  Created by sei on 2012/09/27.
+//  Copyright (c) 2012年 sei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TwitterApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TwitterApp
+//
+//  Created by sei on 2012/09/27.
+//  Copyright (c) 2012年 sei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
